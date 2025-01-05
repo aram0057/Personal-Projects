4f6c9b2084acafa5128e89982269347a3571a833
abbishek <aram0057@student.monash.edu>
2025-01-05 14:14:29 +1100

    test

--- Three js/Transform/src/script.js
+++ Three js/Transform/src/script.js
@@ -24,9 +24,9 @@ scene.add(mesh)
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.x = 3
-camera.position.y = 3
-camera.position.z = 3
+camera.position.x = 1
+camera.position.y = 1
+camera.position.z = 1
 camera.lookAt(mesh.position)
 scene.add(camera)
 
